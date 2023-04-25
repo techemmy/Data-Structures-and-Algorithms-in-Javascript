09c76dcf1555535081483850fe2461fc9132c6d3
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-04-25 22:42:19 +0100

    style: format file

--- Data Structures/LinkedLists/singlyLinkedList.js
+++ Data Structures/LinkedLists/singlyLinkedList.js
@@ -1,135 +1,138 @@
 class SinglyLinkedList {
-    constructor() {
-        this.length = 0;
-        this.head;
-        this.tail;
+  constructor() {
+    this.length = 0;
+    this.head = this.tail;
+  }
+
+  prepend(item) {
+    const newNode = { item };
+
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      this.length++;
+      return;
     }
 
-    prepend(item) {
-        const newNode = {item}
+    const currNode = this.head;
+    newNode.next = currNode;
 
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-            this.length++;
-            return;
+    this.head = newNode;
+    this.length++;
+  }
+
+  insertAt(item, idx) {
+    let currNode = this.head,
+      nextNode = this.head.next,
+      prevNode;
+    const newNode = { item };
+
+    if (idx === 0) {
+      this.prepend(item);
+      return;
+    } else if (idx === this.length) {
+      this.append(item);
+      return;
+    } else if (idx > this.length - 1) {
+      return;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (idx === i) {
+        if (prevNode) {
+          prevNode.next = currNode.next;
+        } else {
+          this.head = currNode.next;
         }
-
-        const currNode = this.head;
         newNode.next = currNode;
+        prevNode.next = newNode;
 
-        this.head = newNode;
         this.length++;
+        return newNode.item;
+      }
+
+      prevNode = currNode;
+      currNode = nextNode;
+      if (currNode) nextNode = currNode.next;
+    }
+  }
+
+  append(item) {
+    const newNode = { item };
+
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      this.length++;
+      return;
     }
 
-    insertAt(item, idx) {
-        let currNode = this.head, nextNode = this.head.next, prevNode;
-        const newNode = {item};
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
 
-        if (idx === 0) {
-            this.prepend(item);
-            return;
-        } else if (idx === this.length) {
-            this.append(item);
-            return;
-        } else if (idx > this.length - 1){
-             return;
+  remove(item) {
+    let currNode = this.head,
+      nextNode = this.head.next,
+      prevNode;
+    for (let i = 0; i < this.length; i++) {
+      if (currNode.item === item) {
+        if (prevNode) {
+          prevNode.next = currNode.next;
+        } else {
+          this.head = currNode.next;
         }
+        this.length--;
+        return currNode.item;
+      }
 
-        for (let i = 0; i < this.length; i++) {
-            if (idx === i) {
-                if (prevNode) {
-                    prevNode.next = currNode.next;
-                } else {
-                    this.head = currNode.next;
-                }
-                newNode.next = currNode;
-                prevNode.next = newNode;
-
-                this.length++;
-                return newNode.item;
-            }
-
-            prevNode = currNode;
-            currNode = nextNode;
-            if (currNode) nextNode = currNode.next;
-        }
+      prevNode = currNode;
+      currNode = nextNode;
+      if (currNode) nextNode = currNode.next;
     }
+  }
 
-    append(item) {
-        const newNode = {item};
+  get(idx) {
+    let currNode = this.head,
+      nextNode = this.head.next;
+    for (let i = 0; i < this.length; i++) {
+      if (idx === i) {
+        return currNode.item;
+      }
 
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-            this.length++;
-            return
+      currNode = nextNode;
+      nextNode = currNode.next;
+    }
+  }
+
+  removeAt(idx) {
+    let currNode = this.head,
+      nextNode = this.head.next,
+      prevNode;
+    for (let i = 0; i < this.length; i++) {
+      if (idx === i) {
+        if (prevNode) {
+          prevNode.next = currNode.next;
+        } else {
+          this.head = currNode.next;
         }
 
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.length++;
+        this.length--;
+        return currNode.item;
+      }
+
+      prevNode = currNode;
+      currNode = nextNode;
+      if (currNode) nextNode = currNode.next;
     }
-
-    remove(item) {
-        let currNode = this.head, nextNode = this.head.next, prevNode;
-        for (let i = 0; i < this.length; i++) {
-            if (currNode.item === item) {
-                if (prevNode) {
-                    prevNode.next = currNode.next;
-                } else {
-                    this.head = currNode.next;
-                }
-                this.length--;
-                return currNode.item;
-            }
-            // console.log("C:",currNode, currNode.next)
-            prevNode = currNode;
-            currNode = nextNode;
-            if (currNode) nextNode = currNode.next;
-        }
-    }
-
-    get(idx) {
-        let currNode = this.head, nextNode = this.head.next;
-        for (let i = 0; i < this.length; i++) {
-            if (idx === i) {
-                return currNode.item;
-            }
-
-            currNode = nextNode;
-            nextNode = currNode.next;
-        }
-    }
-
-    removeAt(idx) {
-        let currNode = this.head, nextNode = this.head.next, prevNode;
-        for (let i = 0; i < this.length; i++) {
-            if (idx === i) {
-                if (prevNode) {
-                    prevNode.next = currNode.next;
-                } else {
-                    this.head = currNode.next;
-                }
-
-                this.length--;
-                return currNode.item;
-            }
-
-            prevNode = currNode;
-            currNode = nextNode;
-            if (currNode) nextNode = currNode.next;
-        }
-    }
-
+  }
 }
 
 const list = new SinglyLinkedList();
 
 // test cases
-list.append(5)
-list.append(7)
-list.append(9)
+list.append(5);
+list.append(7);
+list.append(9);
 
 console.log(list.get(2) === 9);
 console.log(list.removeAt(1) === 7);
@@ -142,20 +145,20 @@ console.log(list.removeAt(0) === 5);
 console.log(list.removeAt(0) === 11);
 console.log(list.length === 0);
 
-list.prepend(5)
-list.prepend(7)
-list.prepend(9)
+list.prepend(5);
+list.prepend(7);
+list.prepend(9);
 
-console.log(list.get(2) === 5)
-console.log(list.get(0) === 9)
-console.log(list.remove(9) === 9)
-console.log(list.length === 2)
-console.log(list.get(0) === 7)
+console.log(list.get(2) === 5);
+console.log(list.get(0) === 9);
+console.log(list.remove(9) === 9);
+console.log(list.length === 2);
+console.log(list.get(0) === 7);
 
-list.insertAt(10, 0)
-console.log(list.get(0) === 10)
-list.insertAt(11, 0)
-console.log(list.get(0) === 11)
-list.insertAt(20, 1)
-console.log(list.get(1) === 20)
-console.log(list.get(2) === 10)
+list.insertAt(10, 0);
+console.log(list.get(0) === 10);
+list.insertAt(11, 0);
+console.log(list.get(0) === 11);
+list.insertAt(20, 1);
+console.log(list.get(1) === 20);
+console.log(list.get(2) === 10);
