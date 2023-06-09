62edde46dfaae19cae71aa07556f34c5d9aa5542
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-09 04:42:00 +0100

    test: add test cases

--- Algorithms/Searching Algorithms/Depth-First Search/BTInOrder.js
+++ Algorithms/Searching Algorithms/Depth-First Search/BTInOrder.js
@@ -20,3 +20,36 @@ function walk(currNode, path) {
 function binaryTreePreOrderSearch(headNode) {
     return walk(headNode, [])
 }
+
+// test case
+const order = binaryTreePreOrderSearch(
+    {
+        value: 7, left: {
+            value: 23,
+            left: {
+                value: 5,
+                left: null,
+                right: null
+            },
+            right: {
+                value: 4,
+                left: null,
+                right: null
+            }
+        },
+        right: {
+            value: 3,
+            left: {
+                value: 18,
+                left: null,
+                right: null
+            },
+            right: {
+                value: 21,
+                left: null,
+                right: null
+            }
+        }
+    }
+)
+console.log(JSON.stringify(order) === JSON.stringify([5, 23, 4, 7, 18, 3, 21]))
